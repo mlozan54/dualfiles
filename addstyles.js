4b959bbf2d675f6d974a109f8fa4430938379fad
Miguel Lozano <mlozan54@xtec.cat>
2024-12-12 08:44:33 +0100

    Update addstyles.js

--- addstyles.js
+++ addstyles.js
@@ -1,1 +1,3 @@
-$('.dcada-stylesheet').append('<link rel="stylesheet" type="text/css" href="https://ioc.xtec.cat/materials/FP/Recursos/fdc/cdndcada/fdc/fp/fp-unitat-estils.css">');
+$(document).ready(function() {
+  $('.dcada-stylesheet').append('<link rel="stylesheet" type="text/css" href="https://ioc.xtec.cat/materials/FP/Recursos/fdc/cdndcada/fdc/fp/fp-unitat-estils.css">');
+  });
